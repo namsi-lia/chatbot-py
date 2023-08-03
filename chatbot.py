a136ca3b21954bd6e18c3fb8b0e78a414474a720
namsi-lia <namsilydia7@gmail.com>
2023-08-03 15:07:09 +0300

    createion of the chatbot UI

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,10 @@
 # Importing required libraries
 from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from flask import Flask, render_template
 
 
+app =Flask(__name__)
 
 bot = ChatBot("chatbot", read_only=False,
               logic_adapters=[
@@ -13,11 +15,20 @@ bot = ChatBot("chatbot", read_only=False,
                   }
               ]
               )
+
+
 trainer =ChatterBotCorpusTrainer(bot)
-trainer.train('chatterbot.corpus.english')
+#trainer.train('chatterbot.corpus.english')
+
+@app.route("/")
+def main():
+    return render_template("index.html")
 
 
 
-while True:
-    user_response =input("User:")
-    print("ChatBot:"+ str (bot.get_response(user_response)))
+#while True:
+    #user_response =input("User:")
+    #print("ChatBot:"+ str (bot.get_response(user_response)))
+
+if __name__=="__main__":
+    app.run(debug=True)
